75afd153d08228967fa950d6a21d74372ad3e38b
PDA <pda@vitol.com>
2024-12-12 15:13:20 +0000

    responsive updates

--- fpl/templates/template.py
+++ fpl/templates/template.py
@@ -44,16 +44,6 @@ def template(
                     flex_grow=1,
                     **styles.template_content_style,
                 ),
-                # rx.flex(
-                #     rx.vstack(
-                #         page_content(),
-                #         width="100%",
-                #         **styles.template_content_style,
-                #     ),
-                #     width="100%",
-                #     **styles.template_page_style,
-                #     max_width=rx.breakpoints(initial="100%", md=styles.max_width),
-                # ),
                 navbar(),
                 flex_direction=rx.breakpoints(initial="column", md="row"),
                 width="100%",

a7f50c05496262f05b33d72f136ed4fd78538fa5
Pete Axtell <axtellpete@gmail.com>
2025-04-12 20:36:48 +0100

    ssl

--- src/rxconfig.py
+++ src/rxconfig.py
@@ -3,5 +3,5 @@ import reflex as rx
 config = rx.Config(
     app_name="fpl",
     state_manager_mode="memory",
-    api_url="http://fpl.axtellcloud.com:8000"
+    api_url="https://fpl.axtellcloud.com:8000"
 )
